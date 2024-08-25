e58b079182065f1178d91870a392042cf241ea9a
metaml <metaml@gmail.com>
2024-08-25 13:17:48 -0700

    add dialog to memory

--- src/model/aws.py
+++ src/model/aws.py
@@ -1,19 +1,26 @@
 import boto3
 import json
+import model.aws as aws
+import model.chat as chat
 
 def openai_api_key() -> str:
   c = boto3.client('secretsmanager')
   return c.get_secret_value(SecretId='openai-api-key')['SecretString']
 
-def publish_aip(msg: str) -> str:
-  aip = "arn:aws:sns:us-east-2:975050288432:aip"
-  return(aip, msg)
+def publish_aip(msg: chat.Message) -> str:
+  aip_arn = "arn:aws:sns:us-east-2:975050288432:aip"
+  return publish(msg, aip_arn)
 
-def publish(msg: str, arn: str, msg_struct: str='json') -> str:
+def publish(msg: chat.Message, arn: str, msg_struct: str='json') -> str:
+  line_json = {'line': msg.content}
+  msg_json = {'default': json.dumps(line_json)}
   c = boto3.client("sns")
-  return c.publish(Message=json.dumps({'default': json.dumps(msg)}),
+  print("### publish: content=", msg.content)
+  print("### publish: payload=", msg_json)
+  return c.publish(Message=json.dumps(msg_json),
+                   MessageStructure=msg_struct,
                    TopicArn=arn,
-                   MessageStructure=msg_struct
-                  )
+                   Subject='ITM'
+                  )['MessageId']
 
                                
--- src/model/chat.py
+++ src/model/chat.py
@@ -1,6 +1,8 @@
 from .globals import clients
+import asyncio as aio
 import fastapi
 import json
+import model.aws as aws
 import os
 import pydantic
 
@@ -14,15 +16,9 @@ class ChatRequest(pydantic.BaseModel):
   messages: list[Message]
   stream: bool = True
 
-@router.post("/chat")
-async def chat_handler(req: ChatRequest):
-  model = "gpt-4o-mini"
-  messages = [{"role": "system",
-               "content": "You are a helpful, kind, empathetic, considerate, intelligent, and rational friend."}
-             ] + req.messages
-
+async def post(req: ChatRequest, messages: list[str], model: str):
   if req.stream:
-    async def response_stream():
+    async def res_stream():
       chat_coroutine = clients["openai"].chat.completions.create(
         model    = model,
         messages = messages,
@@ -30,11 +26,32 @@ async def chat_handler(req: ChatRequest):
       )
       async for event in await chat_coroutine:
         yield json.dumps(event.model_dump(), ensure_ascii=False) + "\n"
-    return fastapi.responses.StreamingResponse(response_stream())
+    return fastapi.responses.StreamingResponse(res_stream())
   else:
-    response = await clients["openai"].chat.completions.create(
+    res = await clients["openai"].chat.completions.create(
       model    = model,
       messages = messages,
-      stream   = False,
+      stream   = req.stream,
     )
-    return response.model_dump()
+    return res.model_dump()
+
+# @todo: implement using async
+async def publish(msgs: list[Message]) -> str:
+  last_msg = None
+  for msg in msgs:
+    print("### msg=", msg)
+    print("### type(msg)=", type(msg))        
+    last_msg = msg
+    aws.publish_aip(msg)
+  return last_msg
+  
+@router.post("/chat")
+async def chat_handler(req: ChatRequest):
+  model = "gpt-4o-mini"
+  messages = [{"role": "system",
+               "content": "You are a helpful, kind, empathetic, considerate, intelligent, and rational friend."}
+             ] + req.messages
+  res: list[str] = await aio.gather(post(req, messages, model), publish(req.messages))
+  print("### res=", res)
+  return res[0]
+
