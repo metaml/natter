1f8d64b36640c466d5ce2dde5398f3d5a6b51a20
metaml <metaml@gmail.com>
2024-09-13 16:06:35 -0700

    sorted dependencies

--- flake.nix
+++ flake.nix
@@ -10,35 +10,32 @@
     utils.lib.eachDefaultSystem ( system:
       let pname    = "ami";
           pversion = "0.1.0.0";
-          pdeps    = [ pkgs.cacert
-                       python
-                       python-pkgs.asyncpg
-                       python-pkgs.boto3
-                       python-pkgs.environs
-                       python-pkgs.fastapi
-                       python-pkgs.gradio
-                       python-pkgs.jinja2
-                       python-pkgs.openai
-                       python-pkgs.passlib
-                       python-pkgs.pydantic-core
-                       python-pkgs.pyjwt
-                       python-pkgs.termcolor
-                       python-pkgs.uvicorn
-                     ];
-          bdeps    = [ pkgs.cacert
-                       python-pkgs.setuptools
-                     ];
-
+          runtime-deps    = [ pkgs.cacert
+                              python
+                              python-pkgs.asyncpg
+                              python-pkgs.boto3
+                              python-pkgs.environs
+                              python-pkgs.fastapi
+                              python-pkgs.gradio
+                              python-pkgs.jinja2
+                              python-pkgs.openai
+                              python-pkgs.passlib
+                              python-pkgs.pydantic-core
+                              python-pkgs.pyjwt
+                              python-pkgs.termcolor
+                              python-pkgs.uvicorn
+                            ];
+          build-deps = with pkgs; [ awscli2
+                                    git
+                                    python-pkgs.setuptools
+                                    docker
+                                    gnumake
+                                    jq
+                                  ];
           pkgs        = nixpkgs.legacyPackages.${system};
           python      = pkgs.python312;
           python-pkgs = pkgs.python312Packages;
 
-          ddeps = with pkgs; [ git # nb: read this should be first--verify later
-                               awscli2
-                               docker
-                               gnumake
-                               jq
-                             ];
           shell-hook = ''
             export LANG=en_US.UTF-8
             export PYTHONPATH=$(pwd)/src:$PYTHONPATH
@@ -53,15 +50,15 @@
              src     = self;
              format  = "other";
              doCheck = false;
-             propagatedBuildInputs = pdeps ++ bdeps;
-             installPhase          = ''mkdir -p $out/bin
-                                       && cp -p ./app/ami.py $out/bin
-                                       && cp -ap src $out/lib
+             propagatedBuildInputs = runtime-deps;
+             installPhase          = ''mkdir -p $out/bin;
+                                       cp -p ./app/ami.py $out/bin;
+                                       cp -ap src $out/lib
                                      '';
            };
            defaultPackage = self.packages.${system}.default;
 
-           devShells.default = pkgs.mkShell { buildInputs = pdeps ++ ddeps;
+           devShells.default = pkgs.mkShell { buildInputs = build-deps ++ runtime-deps;
                                               shellHook = "${shell-hook}";
                                             };
            devShell = self.devShells.${system}.default;
