ccf320b72febe9389c4702d4ddfd0098dbf2177f
metaml <metaml@gmail.com>
2024-09-14 02:47:05 -0700

    wip: added systemd and deploy

--- flake.nix
+++ flake.nix
@@ -2,11 +2,14 @@
   description = "ami rest";
 
   inputs = {
-    nixpkgs = { url = "nixpkgs/nixpkgs-unstable"; };
-    utils   = { url = "github:numtide/flake-utils"; };
+    nixpkgs.url     = "nixpkgs/nixpkgs-unstable";
+    systemd.url     = "github:serokell/systemd-nix";
+    systemd.inputs.nixpkgs.follows = "nixpkgs";
+    deploy.url      = "github:serokell/deploy-rs";
+    utils.url       = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs, systemd, deploy, utils }:
     utils.lib.eachDefaultSystem ( system:
       let name    = "ami";
           version = "0.1.0.0";
@@ -57,6 +60,27 @@
              postFixup = "wrapProgram $out/bin/ami.py --prefix PYTHONPATH : $out/lib --prefix PYTHONPATH : $PYTHONPATH  --prefix PATH : ${python}/bin";
            };
            defaultPackage = self.packages.${system}.default;
+
+           # needed by deploy below
+           apps.default = utils.lib.mkApp { drv = self.packages.${system}.default; };
+
+           # deploy systemd config: nix run
+           inherit (deploy) defaultApp;
+           deploy.nodes.ami = {
+             hostname = "localhost";
+             profiles.ami = {
+               path = systemd.lib.${system}.mkSystemService "ami" {
+                 path = deploy.lib.${system}.setActivate nixpkgs.legacyPackages.${system}.ami "./bin/ami.py";
+                 serviceConfig = {
+                   ExecStart = "ami.py";
+                   Restart   = "always";
+                   Killmode  = "mixed";
+                 };
+                 description = "ami rest service";
+               };
+               activate = "$PROFILE/bin/activate";
+             };
+           };
 
            # docker image
            packages.docker = pkgs.dockerTools.buildImage {
