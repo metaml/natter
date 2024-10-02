c49bf0cb7c63b18fc32fa548dafbe22c201748bc
metaml <metaml@gmail.com>
2024-10-02 01:18:25 -0700

    refomatted code; nb: odd behavior adding a new comment break things

--- flake.nix
+++ flake.nix
@@ -49,63 +49,63 @@
             alias aws='PYTHONPATH= aws'
           '';
       in { # runtime environment
-           packages.default = python.pkgs.buildPythonApplication rec {
-             inherit version;
-             pname   = "${name}";
-             src     = self;
-             format  = "other";
-             doCheck = false;
-             propagatedBuildInputs = runtime-deps;
-             # nb: odd behaviour in that nix build seems to introspect the string below
-             installPhase = "mkdir -p $out/bin; cp -p app/ami.py $out/bin/ami.py; cp -ap src $out/lib";
-             postFixup = "wrapProgram $out/bin/ami.py --prefix PYTHONPATH : $out/lib --prefix PYTHONPATH : $PYTHONPATH  --prefix PATH : ${python}/bin";
-           };
-           defaultPackage = self.packages.${system}.default;
+        packages.default = python.pkgs.buildPythonApplication rec {
+          inherit version;
+          pname   = "${name}";
+          src     = self;
+          format  = "other";
+          doCheck = false;
+          propagatedBuildInputs = runtime-deps;
+          # nb: odd behaviour in that nix build seems to introspect the string below
+          installPhase = "mkdir -p $out/bin; cp -p app/ami.py $out/bin/ami.py; cp -ap src $out/lib";
+          postFixup = "wrapProgram $out/bin/ami.py --prefix PYTHONPATH : $out/lib --prefix PYTHONPATH : $PYTHONPATH  --prefix PATH : ${python}/bin";
+        };
+        defaultPackage = self.packages.${system}.default;
 
-           # needed by deploy below
-           apps.default = utils.lib.mkApp { drv = self.packages.${system}.default; };
+        # needed by deploy below
+        apps.default = utils.lib.mkApp { drv = self.packages.${system}.default; };
 
-           # deploy systemd config: nix run
-           inherit (deploy) defaultApp;
-           deploy.nodes.ami = {
-             hostname = "localhost";
-             profiles.ami = {
-               path = systemd.lib.${system}.mkSystemService "ami" {
-                 path = deploy.lib.${system}.setActivate nixpkgs.legacyPackages.${system}.ami "./bin/ami.py";
-                 serviceConfig = {
-                   ExecStart = "ami.py";
-                   Restart   = "always";
-                   Killmode  = "mixed";
-                 };
-                 description = "ami rest service";
-               };
-               activate = "$PROFILE/bin/activate";
-             };
-           };
+        # deploy systemd config: nix run
+        inherit (deploy) defaultApp;
+        deploy.nodes.ami = {
+          hostname = "localhost";
+          profiles.ami = {
+            path = systemd.lib.${system}.mkSystemService "ami" {
+              path = deploy.lib.${system}.setActivate nixpkgs.legacyPackages.${system}.ami "./bin/ami.py";
+              serviceConfig = {
+                ExecStart = "ami.py";
+                Restart   = "always";
+                Killmode  = "mixed";
+              };
+              description = "ami rest service";
+            };
+            activate = "$PROFILE/bin/activate";
+          };
+        };
 
-           # docker image
-           packages.docker = pkgs.dockerTools.buildImage {
-             inherit name;
-             tag = "latest";
-             created = "now";
-             copyToRoot = pkgs.buildEnv {
-               inherit name;
-               paths = with pkgs; [
-                 bashInteractive
-                 cacert
-                 coreutils
-                 python
-                 self.defaultPackage.${system}
-               ];
-               pathsToLink = [ "/bin" "/usr" ];
-             };
-           };
+        # docker image
+        packages.docker = pkgs.dockerTools.buildImage {
+          inherit name;
+          tag = "latest";
+          created = "now";
+          copyToRoot = pkgs.buildEnv {
+            inherit name;
+            paths = with pkgs; [
+              bashInteractive
+              cacert
+              coreutils
+              python
+              self.defaultPackage.${system}
+            ];
+            pathsToLink = [ "/bin" "/usr" ];
+          };
+        };
 
-           # buld environment
-           devShells.default = pkgs.mkShell { buildInputs = dev-deps ++ runtime-deps;
-                                              shellHook = "${shell-hook}";
-                                            };
-           devShell = self.devShells.${system}.default;
-         }
+        # buld environment
+        devShells.default = pkgs.mkShell { buildInputs = dev-deps ++ runtime-deps;
+                                           shellHook = "${shell-hook}";
+                                         };
+        devShell = self.devShells.${system}.default;
+      }
     );
 }
