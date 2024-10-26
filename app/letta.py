9eac0f4f1787fdcd55017f658c9701af0e6aee3c
metaml <metaml@gmail.com>
2024-10-26 01:16:49 -0700

    letta start script

---
+++ app/letta.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+import os
+import shlex
+import subprocess as subproc
+import sys
+
+if __name__ == '__main__':
+  try:
+    letta = "letta server"
+    arg = shlex.split(uvicorn)
+    res = subproc.run(arg, text=True)
+  except Exception as e:
+    print("exception: ", e, file=sys.stderr)
+    sys.exit(0)
